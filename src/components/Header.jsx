7f1d5f52652c20ac7e36f30bbb210f67f4598d45
9389soriful <134063362+9389soriful@users.noreply.github.com>
2025-06-16 00:51:52 +0600

    added new fature and done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
       <div className="flex items-center gap-2">
         <img
           className="w-[50px] h-[50px] rounded-full p-1 border"
-          src="https://images.unsplash.com/photo-1593882100241-aef1449fe351?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="https://images.unsplash.com/photo-1664188613064-7eea761238f0?q=80&w=2144&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
         />
         <button onClick={handleMenu} className={`cursor-pointer lg:hidden`}>
